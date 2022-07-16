f9614ba4e86c16e0cdbdd5653624fcebea706a14
adw9 <adw9@clemson.edu>
2022-07-16 17:57:43 -0400

    Add Commands.cs

--- bot/CommandHandler.cs
+++ bot/CommandHandler.cs
@@ -4,10 +4,10 @@ using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
+using System.Reflection;
 
 namespace bot
 {
---
+++ bot/Commands.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using System.Reflection;
+
+namespace bot
+{
+    public class CommandList : InteractionModuleBase<SocketInteractionContext>
+    {
+        
+            // dependencies can be accessed through Property injection, public properties with public setters will be set by the service provider
+            public InteractionService Commands { get; set; }
+            private CommandHandler _handler;
+
+            // constructor injection is also a valid way to access the dependecies
+            public CommandList(CommandHandler handler)
+            {
+                _handler = handler;
+            }
+
+            // our first /command!
+            [SlashCommand("8ball", "find your answer!")]
+            public async Task EightBall(string question)
+            {
+                // create a list of possible replies
+                var replies = new List<string>();
+
+                // add our possible replies
+                replies.Add("yes");
+                replies.Add("no");
+                replies.Add("maybe");
+                replies.Add("hazzzzy....");
+
+                // get the answer
+                var answer = replies[new Random().Next(replies.Count - 1)];
+
+                // reply with the answer
+                await RespondAsync($"You asked: [**{question}**], and your answer is: [**{answer}**]");
+            }
+        
+
+
+
+    }
+}
