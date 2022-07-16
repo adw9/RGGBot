abfd3bc16454e4d1c4b092751a23fdf5a69302b8
adw9 <adw9@clemson.edu>
2022-07-15 23:12:26 -0400

    Add CommandHandler, Token moved

--- bot/Program.cs
+++ bot/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Discord;
 using Discord.Net;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+
+using System.Threading;
+using System.IO;
 
 namespace bot
 {
@@ -11,16 +18,31 @@ namespace bot
 	{
 		public static Task Main(string[] args) => new Program().MainAsync();
 
-		public async Task MainAsync()
-		{
-		}
-
 		private Task Log(LogMessage msg)
 		{
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
 		}
 
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+
+            //Reads from a token.txt file in the file folder
+            var token = File.ReadAllText(@"token.txt");
+            
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
 
 
 
@@ -32,6 +54,5 @@ namespace bot
 
 
 
-
-	}
+    }
 }
