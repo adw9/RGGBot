fa0e79247923e3bec5551da3d3cf4784f7e7d0e8
adw9 <adw9@clemson.edu>
2022-07-15 22:33:12 -0400

    Add project files.

---
+++ bot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Net;
+
+namespace bot
+{
+	public class Program
+	{
+		public static Task Main(string[] args) => new Program().MainAsync();
+
+		public async Task MainAsync()
+		{
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
